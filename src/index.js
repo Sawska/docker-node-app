ca14baecd5d5ccb5fc2b7bab7d9065afa9812c35
Alexander Korzh <alexanderkorzh@MacBook-Air-Alexander.local>
2023-08-01 16:15:14 +0300

    Docker deployment

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+
+const app = express();
+
+
+app.get("/",(req,res) => {
+    res.send("Welcome to my server")
+})
+
+
+app.listen(3000,() => {
+    console.log("http://localhost:3000")
+})
